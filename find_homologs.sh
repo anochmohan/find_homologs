e9ff0c0c9d6ab006d3bacf19f3a78b42cfed0e79
Anochmohan <anochmohan@gmail.com>
2023-09-11 03:24:30 -0400

    blast program updated aw/ -outfmt

--- find_homologs.sh
+++ find_homologs.sh
@@ -10,7 +10,7 @@ subject_file="$2"
 output_file="$3"
 
 #BLAST query
-blastn -query $query_file -subject $subject_file -task blastn-short -outfmt '6 sseq' -out $output_file
+tblastn -query $query_file -subject $subject_file -task blastn-short -outfmt '6 std qlen' -out $output_file
 
 #Find perfect match
 match="$(grep -f $query_file $output_file | wc -l)"
